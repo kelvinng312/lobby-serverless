abfac06021347672b4e68e8bf6a8d6badb062b5f
valor <valor312@gmail.com>
2020-06-11 16:13:50 +0800

    Visitor index fix

--- lambdas/websockets/set_name.js
+++ lambdas/websockets/set_name.js
@@ -12,19 +12,38 @@ exports.handler = async (event) => {
   console.log('body', body);
 
   try {
+    // Get Visitor count
+    let maxVisitorIndex = 0;
+    const tableData = await Dynamo.scan(tableName);
+    for (const item of tableData.Items) {
+      if (item.name.includes("Visitor")) {
+        const curVisitorIndex = parseInt(item.name.replace("Visitor", ""));
+        if (curVisitorIndex > maxVisitorIndex) {
+          maxVisitorIndex = curVisitorIndex;
+        }
+      }
+    }
+
+    // Update new name if he is a visitor
+    let newName = body.name;
+    if (newName == "Visitor") {
+      maxVisitorIndex++;
+      newName += maxVisitorIndex;
+    }
+
     // Update self record
     let selfRecord = await Dynamo.get(connectionId, tableName);
-    selfRecord.name = body.name;
+    selfRecord.name = newName;
     const { domainName, stage } = selfRecord;
-
     await Dynamo.write(selfRecord, tableName);
+
 
     // Answer with Id
     await WebSocket.send({
         domainName,
         stage,
         connectionId,
-        message: JSON.stringify({ type: "ans_name", Id: connectionId }),
+        message: JSON.stringify({ type: "ans_name", name: newName, Id: connectionId }),
       });
 
     return Responses._200({ message: "Message received" });
